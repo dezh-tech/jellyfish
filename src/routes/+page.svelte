0175f8d75b9ba1836cfc4f98774b856ef5ef39cd
kehiy <kehiiiiya@gmail.com>
2024-08-28 00:00:47 +0330

    feat: header section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,60 @@
-<h1>Hello, World!</h1>
+<script lang="ts">
+	import logo from '$lib/assets/logo.webp';
+	import hamburger from '$lib/assets/icons/hamburger.svg';
+	import close from '$lib/assets/icons/close.png';
+
+	let isMenuOpen = false;
+
+	const toggleMenu = () => {
+		isMenuOpen = !isMenuOpen;
+	};
+</script>
+
+<main class="relative">
+	<header class="padding-x py-8 absolute z-10 w-full">
+		<nav class="flex justify-between items-center max-container">
+			<a href="/">
+				<img src={logo} alt="jellyfish logo" width={45} height={20} />
+			</a>
+			<b class="font-extrabold text-jelly-blue">JellyFish</b>
+
+			<ul class="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+				<li class="font-montserrat leading-normal text-lg text-slate-gray">
+					<a href="#pricing">Pricing</a>
+				</li>
+				<li class="font-montserrat leading-normal text-lg text-slate-gray">
+					<a href="#features">Features</a>
+				</li>
+				<li class="font-montserrat leading-normal text-lg text-slate-gray">
+					<a href="#contact">Contact Us</a>
+				</li>
+			</ul>
+
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div class="hidden max-lg:block" on:click={toggleMenu}>
+				<img src={isMenuOpen ? close : hamburger} alt="Menu" width={25} height={25} />
+			</div>
+		</nav>
+
+		{#if isMenuOpen}
+			<div class="absolute top-16 left-0 right-0 bg-white shadow-lg z-20 p-4 max-lg:block">
+				<ul class="flex flex-col items-center">
+					<li class="font-montserrat leading-normal text-lg text-slate-gray">
+						<a href="#pricing" on:click={toggleMenu}>Pricing</a>
+					</li>
+					<li class="font-montserrat leading-normal text-lg text-slate-gray">
+						<a href="#features" on:click={toggleMenu}>Features</a>
+					</li>
+					<li class="font-montserrat leading-normal text-lg text-slate-gray">
+						<a href="#contact" on:click={toggleMenu}>Contact Us</a>
+					</li>
+				</ul>
+			</div>
+		{/if}
+	</header>
+
+	<!-- <section class="xl:padding-1 wide:padding-r padding-b">Hero</section> -->
+	<!-- <section class="padding">Features</section> -->
+	<!-- <section class="bg-black padding-x padding-t pb-8">Footer</section> -->
+</main>
