113d81a53cb5d236ff52d0d3f0dd0c2e6971d701
imkarimkarim <imkarimkarim@pm.me>
2025-01-02 13:55:02 +0330

    feat: button variants

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -13,6 +13,27 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ({ className, variant, size, asChild = false, ...props }, ref) => {
         const Comp = asChild ? Slot : "button";
+
+        if (variant === "outline") {
+            return (
+                <div
+                    className={cn(
+                        buttonVariants({ variant, size, className }),
+                        "bg-gradient-to-tr from-[#070C20] to-[#A0C5F7] px-0.5 py-5",
+                    )}
+                >
+                    <Comp
+                        className={cn(
+                            buttonVariants({ variant, size, className }),
+                            "bg-[#151B2A] text-[#D8ECF8]",
+                        )}
+                        ref={ref}
+                        {...props}
+                    />
+                </div>
+            );
+        }
+
         return (
             <Comp
                 className={cn(buttonVariants({ variant, size, className }))}
