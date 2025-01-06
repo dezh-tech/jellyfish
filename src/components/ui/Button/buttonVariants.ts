ca739e4f65b2ec33f9bea9ec886df95b5b525d42
imkarimkarim <imkarimkarim@pm.me>
2025-01-06 18:20:36 +0330

    fix: outline button variant

--- src/components/ui/Button/buttonVariants.ts
+++ src/components/ui/Button/buttonVariants.ts
@@ -9,7 +9,8 @@ export const buttonVariants = cva(
                     "bg-gradient-to-bl from-[hsla(212,73%,81%,1)] to-[hsla(204,69%,90%,1)] text-secondary-foreground shadow-sm hover:bg-secondary/80 rounded-xl",
                 destructive:
                     "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-                outline: "rounded-full font-semibold", // look at buttonVariants.ts for more
+                outline:
+                    "rounded-full font-semibold text-[#D8ECF8] bg-[#161a20] relative after:content-[''] after:-z-10 after:absolute after:w-[100%] after:scale-105 after:h-[100%] after:rounded-full after:bg-[linear-gradient(to_bottom_left,_#a0c5f7,_#070c20)]",
                 secondary: "bg-[#282E4A] text-white",
                 ghost: "hover:bg-accent hover:text-accent-foreground",
                 link: "text-primary underline-offset-4 hover:underline",
