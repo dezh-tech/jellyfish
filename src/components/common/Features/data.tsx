4ea9b3127e642e14244d6a7746c53e25b68e49ef
Zig Blathazar <42387185+ZigBalthazar@users.noreply.github.com>
2025-03-14 15:23:03 +0330

    fix: feature icons. (#61)

--- src/components/common/Features/data.tsx
+++ src/components/common/Features/data.tsx
@@ -1,46 +1,50 @@
-import {
-    InfinityIcon,
-    ShortNameIcon,
-    FastIcon,
-    LightingIcons,
-    RecordsIcon,
-} from "@/assets/icons";
+import { GlobeIcon } from "@/assets/icons/features/GlobeIcon";
+import { LockIcon } from "@/assets/icons/features/LockIcon";
+import { ManageIcon } from "@/assets/icons/features/ManageIcon";
+import { ModernIcon } from "@/assets/icons/features/ModernIcon";
+import { SoftwareIcon } from "@/assets/icons/features/SoftwareIcon";
 
 export const relayFeaturesData = [
     {
-        icon: <InfinityIcon />,
+        icon: <ModernIcon />,
         title: "Modern",
         description:
             "We run a modern relay implementation with maximum NIPs support and on going development.",
     },
     {
-        icon: <ShortNameIcon />,
+        icon: <GlobeIcon />,
         title: "Reliable",
         description:
             "We run multiple instances of our relay over the globe and they are synced meanwhile (deletions and events are 100% synced between them, guaranteed) helping jellyfish to be high-available",
     },
     {
-        icon: <FastIcon />,
+        icon: <ManageIcon />,
         title: "Managed",
         description:
             "We care about your experience, all reports will be checked and we remove and ban harmful contents.",
     },
     {
-        icon: <LightingIcons />,
+        icon: <LockIcon />,
         title: "Privacy and Control",
         description:
             "Jellyfish fully respects to deletion requests (NIP-09), request to vanishes (NIP-62), protected events (NIP-70) and prevent people to read your private gift-wrapped DMs.",
     },
     {
-        icon: <RecordsIcon />,
+        icon: <SoftwareIcon />,
         title: "Custom Software",
         description:
             "We use our own open-source FOSS called Immortal which is 100% custom and dynamic. We are open to feature requests and contributions.",
     },
     // {
-    //     icon: <ShortNameIcon />,
+    //     icon: < />,
     //     title: "Tor Support",
     //     description:
     //         "You can connect to the Jellyfish relay over Tor hidden services as well, enhance your privacy.",
+    // },
+    // {
+    //     icon: < />,
+    //     title: "Advanced full-text search",
+    //     description:
+    //         "You can easily search notes using NIP-50 on jellyfish.",
     // },
 ];
