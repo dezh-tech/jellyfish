1a51460a8cbf657d584e1112fa53b136aaa5b88a
k. <kehiiiiya@gmail.com>
2025-03-17 17:08:31 +0330

    Update alert.ts (#65)

--- src/data/alert.ts
+++ src/data/alert.ts
@@ -1,7 +1,7 @@
 export const alertData = {
-    title: "Notice:",
-    description: "Your session has expired. Please log in again.",
-    linkText: "Show readme.",
-    link: "/maintanance",
-    show: true,
+    title: "",
+    description: "",
+    linkText: "",
+    link: "",
+    show: false,
 };
